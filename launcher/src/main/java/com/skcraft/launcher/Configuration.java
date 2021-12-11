dc5b8bd706ed3488d9495705e1ccb2014ac5e865
Gizmo0320 <gizmo0320@unleveledgaming.com>
2021-12-11 09:25:09 -0600

    Remove permGen

--- launcher/src/main/java/com/skcraft/launcher/Configuration.java
+++ launcher/src/main/java/com/skcraft/launcher/Configuration.java
@@ -28,7 +28,6 @@ public class Configuration {
     private String jvmArgs;
     private int minMemory = 1024;
     private int maxMemory = 0; // Updated in Launcher
-    private int permGen = 256;
     private int windowWidth = 854;
     private int windowHeight = 480;
     private boolean proxyEnabled = false;
